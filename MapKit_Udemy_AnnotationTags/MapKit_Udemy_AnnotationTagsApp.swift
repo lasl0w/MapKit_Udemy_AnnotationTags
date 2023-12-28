3098cdd8a7aa18cfed1c8544133b92d14d315210
Tom Montgomery <montgomery3681@gmail.com>
2023-12-27 22:28:36 -0700

    Initial Commit

---
+++ MapKit_Udemy_AnnotationTags/MapKit_Udemy_AnnotationTagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_Udemy_AnnotationTagsApp.swift
+//  MapKit_Udemy_AnnotationTags
+//
+//  Created by tom montgomery on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_Udemy_AnnotationTagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
